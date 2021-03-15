af871f94d274c525be9cafbd036f8148acc6dd87
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-03-15 19:31:22 +0200

    Add html-loader, file-loader, and clean-webpack-plugin
    - Added html-loader to automatically require the files we reference in img tags
    - Added file-loader to handle the svg,png,jpg,gif files from our images
    - Configured file-loader to add our images to an imgs directory in dist
    - Also configured it to add a hash to their filenames
    - Lastly, added clean-webpack-plugin to our production config to clean out the dist directory each time we build

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,37 +1,46 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, 'dist')
-    },
-    // if we need to remove minification from the code
-    mode: "development",
-    // This option controls if and how source maps are generated.
-    // eval - Each module is executed with eval() and //@ sourceURL.
-        // This is pretty fast. The main disadvantage is that it doesn't
-        // display line numbers correctly since it gets mapped to transpiled code
-        // instead of the original code (No Source Maps from Loaders).
-    devtool: false,
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                  // load in revers order
-                  'style-loader', /*3. Inject CSS into the DOM.*/
-                  'css-loader', /*2. Take your css and turn it into JS*/
-                  'sass-loader'/*1. Turns sass into css*/
-                ]
-            }
+  entry: './src/index.js',
+  // if we need to remove minification from the code
+  mode: 'development',
+  // This option controls if and how source maps are generated.
+  // eval - Each module is executed with eval() and //@ sourceURL.
+  // This is pretty fast. The main disadvantage is that it doesn't
+  // display line numbers correctly since it gets mapped to transpiled code
+  // instead of the original code (No Source Maps from Loaders).
+  devtool: false,
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // load in revers order
+          'style-loader' /*3. Inject CSS into the DOM.*/,
+          'css-loader' /*2. Take your css and turn it into JS*/,
+          'sass-loader' /*1. Turns sass into css*/
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-
+      },
+      {
+        test: /\.html$/,
+        use: ["html-loader"]
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            name: "[name].[contenthash].[ext]",
+            outputPath: "images"
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,47 @@
 const path = require('path')
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: "./src/index.js",
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname, 'dist')
-    },
-    mode: "production",
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                  // load in revers order
-                  'style-loader', /*3. Inject CSS into the DOM.*/
-                  'css-loader', /*2. Take your css and turn it into JS*/
-                  'sass-loader'/*1. Turns sass into css*/
-                ]
-            }
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          // load in revers order
+          'style-loader' /*3. Inject CSS into the DOM.*/,
+          'css-loader' /*2. Take your css and turn it into JS*/,
+          'sass-loader' /*1. Turns sass into css*/
         ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./src/index.html"
-        })
-    ],
-
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: "[name].[contenthash].[ext]",
+              outputPath: "images"
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 }
