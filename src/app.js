c24c2f8ff1bf429a1eea0452a4fed85853fb0091
mohammdelzanaty <mohamed.elzanaty3@vodafone.com>
2021-03-15 18:17:04 +0200

    Webpack now bundling all our app code
    - We use import/export to indicate dependencies
    - Webpack makes sure everything loads in the correct order
    - We can remove all our additional script tags in index.html

--- src/app.js
+++ src/app.js
@@ -1,24 +1,21 @@
-const alertService = new AlertService()
-const componentService = new ComponentService()
+import { parseInputs } from './utils/parse-inputs'
+import { inputsAreValid } from "./utils/inputs-are-valid";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const run = (alertService, componentService) => {
+export const run = (alertService, componentService) => {
+  alertService.hideErrors()
+
+  componentService.onClick(() => {
     alertService.hideErrors()
-
-    componentService.onClick(() => {
-      alertService.hideErrors()
-      const inputs = componentService.getInputs()
-      // convert inputs to numbers
-      const parsedInputs = parseInputs(...inputs)
-      // check if the input is valid numbers or not
-      if (inputsAreValid(...parsedInputs)) {
-        const [numA, numB] = parsedInputs
-        componentService.setResult(numA + numB)
-      } else {
-        componentService.setResult('')
-        alertService.handleAdditionError(inputs, parsedInputs)
-      }
-    })
-  }
-  run(alertService, componentService)
-})
+    const inputs = componentService.getInputs()
+    // convert inputs to numbers
+    const parsedInputs = parseInputs(...inputs)
+    // check if the input is valid numbers or not
+    if (inputsAreValid(...parsedInputs)) {
+      const [numA, numB] = parsedInputs
+      componentService.setResult(numA + numB)
+    } else {
+      componentService.setResult('')
+      alertService.handleAdditionError(inputs, parsedInputs)
+    }
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-alert('Welcome Webpack')
+import { run } from './app'
+import { AlertService } from './services/alert.service'
+import { ComponentService } from './services/component.service'
+
+const alertService = new AlertService()
+const componentService = new ComponentService()
+
+document.addEventListener('DOMContentLoaded', () => {
+  run(alertService, componentService)
+})
